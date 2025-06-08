2ac70cebd052e8ec685b7148663ce9d3246e2655
Ezequiel Madrid <madridezequiel299@gmail.com>
2025-06-08 20:30:02 -0300

    preparing socket file

---
+++ server/src/lib/socket.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
+
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId]?.socketId;
+}
+
+const userSocketMap = {}; // { userId: { socketId, username } }
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  const username = socket.handshake.query.username;
+  if (userId) {
+    userSocketMap[userId] = {
+      socketId: socket.id,
+      username,
+    };
+  }
+  console.log(`${username} is Online`);
+  io.emit("getFriends", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log(`${username} is Offline`);
+    // Remove the user from the userSocketMap when they
+    delete userSocketMap[userId];
+    io.emit("getFriends", Object.keys(userSocketMap));
+  });
+});
+
+export { io, app, server };
