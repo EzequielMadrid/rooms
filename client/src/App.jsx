aff9c092ceddde1493bc2b6d34ecac864a1409b2
Ezequiel Madrid <madridezequiel299@gmail.com>
2025-06-08 20:41:52 -0300

    match routes of client and server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,56 @@
+import { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
+import HubPage from "./pages/HubPage";
+import SignUpPage from "./pages/SignUpPage";
+import SigninPage from "./pages/SigninPage";
+import ConfigPage from "./pages/ConfigPage";
+import AvatarPage from "./pages/AvatarPage";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const { theme } = useThemeStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  console.log({ onlineUsers });
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-pulse" />
+      </div>
+    );
 
   return (
     <div data-theme={theme}>
-      <h1>Welcome to the Chat App</h1>
-      <p>Current theme: {theme}</p>
-      {/* Other components can be added here */}
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HubPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signin"
+          element={!authUser ? <SigninPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<ConfigPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <AvatarPage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Toaster />
     </div>
   );
 }
