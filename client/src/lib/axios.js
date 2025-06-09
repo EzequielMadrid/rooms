9e97fd42ce39e29349638989547a3c90c8915097
Ezequiel Madrid <madridezequiel299@gmail.com>
2025-06-09 12:59:03 -0300

    x

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,10 +1,13 @@
+import axios from "axios";
+
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : import.meta.env.VITE_BACKEND_URL
+    ? `${import.meta.env.VITE_BACKEND_URL}/api`
+    : "/api";
+
 export const axiosInstance = axios.create({
-  // baseURL: "http://localhost:3000/api",
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : import.meta.env.VITE_BACKEND_URL
-      ? `${import.meta.env.VITE_BACKEND_URL}/api`
-      : "/api",
+  baseURL,
   withCredentials: true,
 });
