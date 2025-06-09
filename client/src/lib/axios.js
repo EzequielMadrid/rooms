42e700bb12f32ad6a3a4465caa3bc0671a74cb1e
Ezequiel Madrid <madridezequiel299@gmail.com>
2025-06-09 13:11:00 -0300

    vite.config

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "http://localhost:3000/api",
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "/api",
+  baseURL: "/api",
   withCredentials: true,
 });
