c4fe8d9d94507cd75e6224e99086b63cf07f8823
Ezequiel Madrid <madridezequiel299@gmail.com>
2025-06-09 12:55:59 -0300

    vite backend url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,10 +1,10 @@
-import axios from "axios";
-
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:3000/api",
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
+      : import.meta.env.VITE_BACKEND_URL
+      ? `${import.meta.env.VITE_BACKEND_URL}/api`
       : "/api",
   withCredentials: true,
 });
